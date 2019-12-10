b41151e7c8201535044837846c161cac62daef71
Eric Hornby <erich@frost-fighter.local>
2019-12-10 14:52:10 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>Weather App!</p>
+      </header>
+    </div>
+  );
+}
